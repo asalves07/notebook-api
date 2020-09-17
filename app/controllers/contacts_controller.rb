02e16b26b2cfb217e5294dc420d00e5517312e91
Andre Souza Alves <alves.andre.ds@gmail.com>
2020-09-16 23:16:04 -0300

    deserialization

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -46,10 +46,11 @@ class ContactsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def contact_params
-      params.require(:contact).permit(
-        :name, :email, :birthdate, :kind_id, 
-        phones_attributes: [:id, :number, :_destroy],
-        address_attributes: [:id, :street, :number, :district, :city, :uf, :zip]
-        )
+      # params.require(:contact).permit(
+      #   :name, :email, :birthdate, :kind_id, 
+      #   phones_attributes: [:id, :number, :_destroy],
+      #   address_attributes: [:id, :street, :number, :district, :city, :uf, :zip]
+      #   )
+      ActiveModelSerializers::Deserialization.jsonapi_parse(params)
     end
 end
